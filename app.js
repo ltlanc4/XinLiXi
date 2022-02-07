e63c3b2b7584c9a355fc5d1965d46bdecd89965b
ltlanc4 <ltlanc4@gmail.com>
2022-02-07 15:50:28 +0700

    Len di nao

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const App = (function () {
             event.preventDefault();
             if (event.target.classList.contains("btn-yes") || event.target.classList.contains("btn-yes-du-phong")) {
                 document.querySelector(".text-success").classList.remove("d-none");
+                document.querySelector("#reset").classList.remove("d-none");
             }
             if (event.target.classList.contains("btn-no")) {
                 document.querySelector('#ban-dau').classList.add("d-none");
@@ -24,7 +25,16 @@ const App = (function () {
                 document.querySelector('#ban-dau').classList.remove("d-none");
                 document.querySelector('#du-phong').classList.add("d-none");
             }
-        })
+        });
+        document.querySelector("body").addEventListener('click', event => {
+            event.preventDefault();
+            if (event.target.classList.contains("btn-reset")) {
+                document.querySelector(".text-success").classList.add("d-none");
+                document.querySelector('#ban-dau').classList.remove("d-none");
+                document.querySelector('#du-phong').classList.add("d-none");
+                document.querySelector("#reset").classList.add("d-none");
+            }
+        });
     }
     return {
         init,
